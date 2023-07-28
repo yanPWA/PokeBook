a539a61bb900101d66df52b87278b20d0cc8e193
藤田彰子 <akiko@yanMacBook-Air.local>
2023-07-29 01:56:25 +0900

    [fix]ダークテーマ時に見えなくなっていた戻アイコンを修正

--- app/src/main/java/com/example/pokebook/ui/screen/PokemonDetailScreen.kt
+++ app/src/main/java/com/example/pokebook/ui/screen/PokemonDetailScreen.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
@@ -22,16 +22,13 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
@@ -45,7 +42,6 @@ import com.example.pokebook.R
 import com.example.pokebook.ui.viewModel.PokemonDetailScreenUiData
 import com.example.pokebook.ui.viewModel.PokemonDetailUiState
 import com.example.pokebook.ui.viewModel.PokemonDetailViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 @Composable
@@ -98,7 +94,7 @@ private fun PokemonDetailScreen(
     ) {
         Image(
             imageVector = ImageVector.vectorResource(
-                id = R.drawable.arrow_back_fill0_wght400_grad0_opsz48
+                id = if (isSystemInDarkTheme()) R.drawable.arrow_back_fillf else R.drawable.arrow_back_fill0
             ),
             contentDescription = null,
             modifier = Modifier
