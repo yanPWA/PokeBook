1f6a9a9eb30831a377216252eb6b56de611b32d5
藤田彰子 <akiko@yanMacBook-Air.local>
2023-08-04 17:14:45 +0900

    [add]TypeNamに関する拡張関数を追加

--- app/src/main/java/com/example/pokebook/ui/screen/TypeTag.kt
+++ app/src/main/java/com/example/pokebook/ui/screen/TypeTag.kt
@@ -123,25 +123,51 @@ fun TypeTag(
  * こおり
  * 未知
  */
-enum class TypeName(val typeName: String, val jaTypeName: String, val number:String) {
-    NORMAL("normal","ノーマル","1"),
-    FIGHTING("fighting","かくとう","2"),
-    POISON("poison","どく","4"),
-    GROUND("ground","じめん","5"),
-    FLYING("flying","ひこう","3"),
-    PSYCHIC("psychic","エスパー","14"),
-    BUG("bug","むし","7"),
-    ROCK("rock","いわ","6"),
-    GHOST("ghost","ゴースト","8"),
-    DRAGON("dragon","ドラゴン","16"),
-    DARK("dark","あく","17"),
-    STEEL("steel","はがね","9"),
-    FAIRY("fairy","フェアリー","18"),
-    FIRE("fire","ほのお","10"),
-    WATER("water","みず","11"),
-    ELECTRIC("electric","でんき","13"),
-    GRASS("grass","くさ","12"),
-    SHADOW("shadow","ダーク","10002"),
-    ICE("ice","こおり","15"),
-    UNKNOWN("unknown","未知","10001")
+enum class TypeName(
+    val typeName: String,
+    val jaTypeName: String,
+    val number: String,
+    val color: Color
+) {
+    NORMAL("normal", "ノーマル", "1", Color(color = 0xFFAEAEAE)),
+    FIGHTING("fighting", "かくとう", "2", Color(color = 0xFFEE6969)),
+    POISON("poison", "どく", "4", Color(color = 0xFFAB7ACA)),
+    GROUND("ground", "じめん", "5", Color(color = 0xFFC8A841)),
+    FLYING("flying", "ひこう", "3", Color(color = 0xFF64A7F1)),
+    PSYCHIC("psychic", "エスパー", "14", Color(color = 0xFF9AC30E)),
+    BUG("bug", "むし", "7", Color(color = 0xFF51CB5A)),
+    ROCK("rock", "いわ", "6", Color(color = 0xFFFAC727)),
+    GHOST("ghost", "ゴースト", "8", Color(color = 0xFF756EB4)),
+    DRAGON("dragon", "ドラゴン", "16", Color(color = 0xFF9AC30E)),
+    DARK("dark", "あく", "17", Color(color = 0xFFFF8859)),
+    STEEL("steel", "はがね", "9", Color(color = 0xFF818AA4)),
+    FAIRY("fairy", "フェアリー", "18", Color(color = 0xFFFC7799)),
+    FIRE("fire", "ほのお", "10", Color(color = 0xFFFFA766)),
+    WATER("water", "みず", "11", Color(color = 0xFF64C5F7)),
+    ELECTRIC("electric", "でんき", "13", Color(color = 0xFFE7D400)),
+    GRASS("grass", "くさ", "12", Color(color = 0xFF9AC30E)),
+    SHADOW("shadow", "ダーク", "10002", Color(color = 0xFF333333)),
+    ICE("ice", "こおり", "15", Color(color = 0xFF60E9F5)),
+    UNKNOWN("unknown", "未知", "10001", Color(color = 0xFFAEAEAE)),
+}
+
+/**
+ * 日本語名 -> TypeNumber
+ */
+fun String.convertToTypeNumber(): String {
+    return TypeName.values().find { it.jaTypeName == this }?.number ?: ""
+}
+
+/**
+ * TypeNumber -> 日本語名
+ */
+fun String.convertToJaTypeName(): String {
+    return TypeName.values().find { it.number == this }?.jaTypeName ?: ""
+}
+
+/**
+ * 日本語名 -> カラーコードに変換
+ */
+fun String.convertToJaColorCode(): Color {
+    return TypeName.values().find { it.jaTypeName == this }?.color ?: Color(color = 0xFFAEAEAE)
 }
