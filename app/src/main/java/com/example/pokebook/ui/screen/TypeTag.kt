5472dcf792bd92341150f8ce604d080e386d5f1a
藤田彰子 <akiko@yanMacBook-Air.local>
2023-08-07 14:37:43 +0900

    [add]typeTag周りの処理を整理

--- app/src/main/java/com/example/pokebook/ui/screen/TypeTag.kt
+++ app/src/main/java/com/example/pokebook/ui/screen/TypeTag.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,8 +15,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pokebook.R
-import com.example.pokebook.ui.viewModel.PokemonDetailScreenUiData
-import java.lang.reflect.Type
 
 @Preview(showBackground = true)
 @Composable
@@ -26,6 +23,9 @@ fun TypeTagPreview() {
 }
 
 
+/**
+ * typeタグの生成
+ */
 @Composable
 fun TypeTag(
     typeList: List<String>,
@@ -45,53 +45,11 @@ fun TypeTag(
         )
         typeList.forEach { type ->
             Text(
-                text = when (type) {
-                    TypeName.FIGHTING.typeName -> stringResource(R.string.type_name_fighting)
-                    TypeName.POISON.typeName -> stringResource(R.string.type_name_poison)
-                    TypeName.GROUND.typeName -> stringResource(R.string.type_name_ground)
-                    TypeName.FLYING.typeName -> stringResource(R.string.type_name_flying)
-                    TypeName.PSYCHIC.typeName -> stringResource(R.string.type_name_psychic)
-                    TypeName.BUG.typeName -> stringResource(R.string.type_name_bug)
-                    TypeName.ROCK.typeName -> stringResource(R.string.type_name_rock)
-                    TypeName.GHOST.typeName -> stringResource(R.string.type_name_ghost)
-                    TypeName.DRAGON.typeName -> stringResource(R.string.type_name_dragon)
-                    TypeName.DARK.typeName -> stringResource(R.string.type_name_dark)
-                    TypeName.STEEL.typeName -> stringResource(R.string.type_name_steel)
-                    TypeName.FAIRY.typeName -> stringResource(R.string.type_name_fairy)
-                    TypeName.FIRE.typeName -> stringResource(R.string.type_name_fire)
-                    TypeName.WATER.typeName -> stringResource(R.string.type_name_water)
-                    TypeName.ELECTRIC.typeName -> stringResource(R.string.type_name_electric)
-                    TypeName.GRASS.typeName -> stringResource(R.string.type_name_grass)
-                    TypeName.SHADOW.typeName -> stringResource(R.string.type_name_shadow)
-                    TypeName.ICE.typeName -> stringResource(R.string.type_name_ice)
-                    TypeName.NORMAL.typeName -> stringResource(R.string.type_name_normal)
-                    TypeName.UNKNOWN.typeName -> stringResource(R.string.type_name_unknown)
-                    else -> type
-                },
+                text = type.convertToJaTypeNameByTypeName(),
                 modifier = modifier
                     .padding(1.dp)
                     .background(
-                        color = when (type) {
-                            TypeName.FIGHTING.typeName -> Color(color = 0xFFEE6969)
-                            TypeName.POISON.typeName -> Color(color = 0xFFAB7ACA)
-                            TypeName.GROUND.typeName -> Color(color = 0xFFC8A841)
-                            TypeName.FLYING.typeName -> Color(color = 0xFF64A7F1)
-                            TypeName.PSYCHIC.typeName -> Color(color = 0xFF9AC30E)
-                            TypeName.BUG.typeName -> Color(color = 0xFF51CB5A)
-                            TypeName.ROCK.typeName -> Color(color = 0xFFFAC727)
-                            TypeName.GHOST.typeName -> Color(color = 0xFF756EB4)
-                            TypeName.DRAGON.typeName -> Color(color = 0xFF9AC30E)
-                            TypeName.DARK.typeName -> Color(color = 0xFFFF8859)
-                            TypeName.STEEL.typeName -> Color(color = 0xFF818AA4)
-                            TypeName.FAIRY.typeName -> Color(color = 0xFFFC7799)
-                            TypeName.FIRE.typeName -> Color(color = 0xFFFFA766)
-                            TypeName.WATER.typeName -> Color(color = 0xFF64C5F7)
-                            TypeName.ELECTRIC.typeName -> Color(color = 0xFFE7D400)
-                            TypeName.GRASS.typeName -> Color(color = 0xFF9AC30E)
-                            TypeName.SHADOW.typeName -> Color(color = 0xFF333333)
-                            TypeName.ICE.typeName -> Color(color = 0xFF60E9F5)
-                            else -> Color(color = 0xFFAEAEAE) //NORMAL,UNKNOWN
-                        },
+                        color = type.convertToColorCodeByTypeName(),
                         shape = RoundedCornerShape(5.dp)
                     )
                     .padding(2.dp),
@@ -166,8 +124,22 @@ fun String.convertToJaTypeName(): String {
 }
 
 /**
+ * typeName -> 日本語名
+ */
+fun String.convertToJaTypeNameByTypeName(): String {
+    return TypeName.values().find { it.typeName == this }?.jaTypeName ?: ""
+}
+
+/**
  * 日本語名 -> カラーコードに変換
  */
-fun String.convertToJaColorCode(): Color {
+fun String.convertToColorCode(): Color {
     return TypeName.values().find { it.jaTypeName == this }?.color ?: Color(color = 0xFFAEAEAE)
+}
+
+/**
+ * typeName -> カラーコード
+ */
+fun String.convertToColorCodeByTypeName(): Color {
+    return TypeName.values().find { it.typeName == this }?.color ?: Color(color = 0xFFAEAEAE)
 }
