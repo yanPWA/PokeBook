7fd7e2f52f2e8a22e30c9836ccf48c60a61ad048
藤田彰子 <akiko@yanMacBook-Air.local>
2023-08-08 16:28:38 +0900

    [refactor] CommonScreenの整理

--- app/src/main/java/com/example/pokebook/ui/screen/PokemonDetailScreen.kt
+++ app/src/main/java/com/example/pokebook/ui/screen/PokemonDetailScreen.kt
@@ -22,14 +22,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -303,5 +308,47 @@ private fun PokemonDetailScreenPreview() {
     PokemonDetailScreen(
         uiData = PokemonDetailScreenUiData(),
         onClickBackButton = {},
+    )
+}
+
+/**
+ * 文字サイズの自動調整
+ */
+@Composable
+fun AutoSizeableText(
+    text: String,
+    color: Color,
+    maxTextSize: Int = 50,
+    minTextSize: Int = 40,
+    modifier: Modifier
+) {
+    var textSize by remember { mutableStateOf(maxTextSize) }
+    val checked = remember(text) { mutableMapOf<Int, Boolean?>() }
+    var overflow by remember { mutableStateOf(TextOverflow.Clip) }
+
+    androidx.compose.material.Text(
+        text = text,
+        color = color,
+        fontSize = textSize.sp,
+        maxLines = 1,
+        overflow = overflow,
+        modifier = modifier,
+        onTextLayout = {
+            if (it.hasVisualOverflow) {
+                checked[textSize] = true
+                if (textSize > minTextSize) {
+                    textSize -= 1
+                } else {
+                    overflow = TextOverflow.Ellipsis
+                }
+            } else {
+                checked[textSize] = false
+                if (textSize < maxTextSize) {
+                    if (checked[textSize + 1] == null) {
+                        textSize += 1
+                    }
+                }
+            }
+        }
     )
 }
