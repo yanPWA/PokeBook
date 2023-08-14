91520f1a1268149d1ba98560d1876d67278bdbda
藤田彰子 <akiko@yanMacBook-Air.local>
2023-08-14 16:15:27 +0900

    フォーマット修正

--- app/src/main/java/com/example/pokebook/data/OfflineLikesRepository.kt
+++ app/src/main/java/com/example/pokebook/data/OfflineLikesRepository.kt
@@ -1,11 +1,14 @@
 package com.example.pokebook.data
 
+import android.util.Log
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
 
 class OfflineLikesRepository(private val likeDao: LikeDao) : LikesRepository {
-    override  fun getAllItemsStream(): Flow<List<Like>> =likeDao.getAllItems()
+    override fun getAllItemsStream(): Flow<List<Like>> = likeDao.getAllItems()
 
-    override suspend fun insertItem(like: Like)  = likeDao.insert(like)
+    override suspend fun insertItem(like: Like) = likeDao.insert(like)
 
     override suspend fun deleteItem(like: Like) = likeDao.delete(like)
 
