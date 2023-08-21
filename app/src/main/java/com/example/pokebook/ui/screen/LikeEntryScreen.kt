b06b8adf84d9c2c5eb2fdb1912bf6c8d0c875c24
藤田彰子 <akiko@yanMacBook-Air.local>
2023-08-21 15:43:09 +0900

    [clean]when文の順番を変更

--- app/src/main/java/com/example/pokebook/ui/screen/LikeEntryScreen.kt
+++ app/src/main/java/com/example/pokebook/ui/screen/LikeEntryScreen.kt
@@ -110,12 +110,11 @@ private fun LikeEntryBody(
     }
 
     when (state) {
+        is LikeUiState.Loading -> {
+            LoadingScreen()
+        }
         is LikeUiState.Fetched -> {
-            if ((state as LikeUiState.Fetched).uiDataList.isEmpty()) {
-                PokemonNotFound(
-                    onClickBackSearchScreen = onClickBackButton
-                )
-            } else {
+            if ((state as LikeUiState.Fetched).uiDataList.isNotEmpty()) {
                 LikeScreen(
                     likeList = (state as LikeUiState.Fetched).uiDataList,
                     onClickCard = onClickCard,
@@ -124,13 +123,12 @@ private fun LikeEntryBody(
                     getAllList = getAllList,
                     getPokemonSpecies = getPokemonSpecies
                 )
+            } else {
+                PokemonNotFound(
+                    onClickBackSearchScreen = onClickBackButton
+                )
             }
         }
-
-        is LikeUiState.Loading -> {
-            LoadingScreen()
-        }
-
         is LikeUiState.ResultError -> {
             ResultError(onClickBackSearchScreen = onClickBackButton)
         }
